eb9e52e3d2e7431e3232769ae62c79b327f80aa5
padams <padams@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-20 21:42:09 +0000

    fixed broken namespace on product name.
    git-svn-id: https://plugins.svn.wordpress.org/photopress-paypal-shopping-cart/trunk@1202642 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- photopress-paypal-shopping-cart.php
+++ photopress-paypal-shopping-cart.php
@@ -5,7 +5,7 @@ Plugin Name: PhotoPress - Paypal Shopping Cart
 Plugin URI: Permalink: http://www.photopressdev.com
 Description: Dynamicaly adds shopping cart functionality to Image Attachments, Posts, or Pages. Utilizes the WordPress Simple Paypal Shopping Cart Plugin.
 Author: Peter Adams
-Version: 1.4
+Version: 1.5
 Author URI: http://www.photopressdev.com 
 */
 
@@ -209,7 +209,7 @@ class papt_spsc {
 		$form .= '</select>';
 		$form .= '<br />';
 		$form .= sprintf('<input type="submit" value="%s" />', $addToCartLabel);
-		$form .= sprintf('<input type="hidden" name="product" value="%s" />', $product_name);
+		$form .= sprintf('<input type="hidden" name="wspsc_product" value="%s" />', $product_name);
 		$form .= '<input type="hidden" name="price" value="0" />';
 		$form .= sprintf('<input type="hidden" name="product_tmp" value="%s" />', $product_name);
 		$form .= sprintf('<input type="hidden" name="shipping" value="%s" />', $shipping );
